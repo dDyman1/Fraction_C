fb1a7bd9ffee44cdbac4110a5ad859aa0b9cee64
dDyman1 <ddyman@student.touro.edu>
2021-02-17 12:10:59 -0500

    Template Creation

--- FractionTesting/UnitTest1.cs
+++ FractionTesting/UnitTest1.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FractionClassroom;
+using FractionNS;
 using System;
 
 namespace FractionTesting
@@ -7,6 +7,9 @@ namespace FractionTesting
     [TestClass]
     public class UnitTest1
     {
+        Fraction f1 = new Fraction(3, 5);
+        Fraction f2 = new Fraction(2, 7);
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void NewFraction1()
@@ -23,18 +26,7 @@ namespace FractionTesting
             Assert.AreEqual(fraction.Denominator, 1);
         }
 
-        [TestMethod]
-        public void NewFraction3()
-        {
-            Fraction fraction = new Fraction(6, 14);
-            Assert.AreEqual(fraction.Numerator, 3);
-            Assert.AreEqual(fraction.Denominator, 7);
-        }
-
-        Fraction f1 = new Fraction(3, 5);
-        Fraction f2 = new Fraction(2, 7);
-
-        [TestMethod]
+       [TestMethod]
         public void TwoFractionAdd()
         {
             Fraction f = f1 + f2;
@@ -65,118 +57,6 @@ namespace FractionTesting
             Assert.AreEqual(f.Numerator, 21);
             Assert.AreEqual(f.Denominator, 10);
         }
-
-        [TestMethod]
-        public void IntFractionAdd()
-        {
-            Fraction f = f1 + 3;
-            Assert.AreEqual(f.Numerator, 18);
-            Assert.AreEqual(f.Denominator, 5);
-        }
-
-        [TestMethod]
-        public void IntFractionSub()
-        {
-            Fraction f = f1 - 3;
-            Assert.AreEqual(f.Numerator, -12);
-            Assert.AreEqual(f.Denominator, 5);
-        }
-
-        [TestMethod]
-        public void IntFracMultiply()
-        {
-            Fraction f = f1 * 3;
-            Assert.AreEqual(f.Numerator, 9);
-            Assert.AreEqual(f.Denominator, 5);
-        }
-
-        [TestMethod]
-        public void IntFracDivide()
-        {
-            Fraction f = f1 / 3;
-            Assert.AreEqual(f.Numerator, 1);
-            Assert.AreEqual(f.Denominator, 5);
-        }
-
-        [TestMethod]
-        public void DoubleFractionAdd()
-        {
-            Fraction f = f1 + 0.1;
-            Assert.AreEqual(f.Numerator, 7);
-            Assert.AreEqual(f.Denominator, 10);
-        }
-
-        [TestMethod]
-        public void DoubleFractionSub()
-        {
-            Fraction f = f1 - 3.67;
-            Assert.AreEqual(f.Numerator, -307);
-            Assert.AreEqual(f.Denominator, 100);
-        }
-
-        [TestMethod]
-        public void DoubleFracMultiply()
-        {
-            Fraction f = f1 * 3.25;
-            Assert.AreEqual(f.Numerator, 39);
-            Assert.AreEqual(f.Denominator, 20);
-        }
-
-        [TestMethod]
-        public void DoubleFracDivide()
-        {
-            Fraction f = f1 / 2.71;
-            Assert.AreEqual(f.Numerator, 60);
-            Assert.AreEqual(f.Denominator, 271);
-        }
-
-        [TestMethod]
-
-        public void GreaterThanTest()
-        {
-            Assert.AreEqual(f1 > f2, true);
-            Assert.AreEqual(f2 > f1, false);
-        }
-
-        [TestMethod]
-        public void GreaterOrEqTest()
-        {
-            Assert.AreEqual(f1 >= f2, true);
-            Assert.AreEqual(f2 >= f1, false);
-            Assert.AreEqual(f1 >= 0.6, true);
-        }
-
-        [TestMethod]
-
-        public void LessThanTest()
-        {
-            Assert.AreEqual(f1 < f2, false);
-            Assert.AreEqual(f2 < f1, true);
-        }
-
-        [TestMethod]
-        public void LessOrEqTest()
-        {
-            Assert.AreEqual(f1 <= f2, false);
-            Assert.AreEqual(f2 <= f1, true);
-            Assert.AreEqual(f1 <= 0.6, true);
-        }
-
-        [TestMethod]
-
-        public void EqualToTest()
-        {
-            Assert.AreEqual(f1 == f2, false);
-            Assert.AreEqual(f1 == 0.6, true);
-        }
-
-        [TestMethod]
-        public void NotEqualToTest()
-        {
-            Assert.AreEqual(f1 != f2, true);
-            Assert.AreEqual(f1 != 0.6, false);
-        }
-
 
     }
 }
--- HelloGithubClassroom/Fraction.cs
+++ HelloGithubClassroom/Fraction.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
-namespace FractionClassroom
+namespace FractionNS
 {
     public class Fraction
     {
@@ -11,10 +11,10 @@ namespace FractionClassroom
         public static implicit operator Fraction(double val)
         {
             String str = val + "";
-            int subLength = str.Substring(str.IndexOf(".")+1).Length;
+            int subLength = str.Substring(str.IndexOf(".") + 1).Length;
             int var = Int32.Parse(val.ToString().Replace(".", ""));
             subLength = (int)Math.Pow(10, subLength);
-            return new Fraction(var, subLength); 
+            return new Fraction(var, subLength);
         }
         public static explicit operator double(Fraction f) => (double)f.Numerator / f.Denominator;
         public static explicit operator int(Fraction f) => f.Numerator / f.Denominator;
@@ -50,6 +50,7 @@ namespace FractionClassroom
             }
         }
 
+
         static int gcdVal(int a, int b)
         {
             if (b == 0)
@@ -73,92 +74,17 @@ namespace FractionClassroom
                 d = value;
             }
         }
+    }
 
         //Overloading Operators
 
-        public static Fraction operator +(Fraction f1, Fraction f2)
-        {
-            int numer = (f1.Numerator * f2.Denominator) + (f2.Numerator * f1.Denominator);
-            int denum = f1.Denominator * f2.Denominator;
-            return new Fraction(numer, denum);
-        }
-
-        public static Fraction operator -(Fraction f1, Fraction f2)
-        {
-            int numer = (f1.Numerator * f2.Denominator) - (f2.Numerator * f1.Denominator);
-            int denum = f1.Denominator * f2.Denominator;
-            return new Fraction(numer, denum);
-        }
-
-        public static Fraction operator *(Fraction f1, Fraction f2)
-        {
-            int numer = f1.Numerator * f2.Numerator;
-            int denum = f1.Denominator * f2.Denominator;
-            return new Fraction(numer, denum);
-        }
-
-        public static Fraction operator /(Fraction f1, Fraction f2)
-        {
-            int numer = f1.Numerator * f2.Denominator;
-            int denum = f1.Denominator * f2.Numerator;
-            return new Fraction(numer, denum);
-        }
-
-        public static bool operator >(Fraction f1, Fraction f2)
-        {
-            double v1 = (double)f1;
-            double v2 = (double)f2;
-            return v1 > v2;
-
-        }
-
-        public static bool operator <(Fraction f1, Fraction f2)
-        {
-            double v1 = (double)f1;
-            double v2 = (double)f2;
-            return v1 < v2;
-
-        }
-
-        public static bool operator >=(Fraction f1, Fraction f2)
-        {
-            double v1 = (double)f1;
-            double v2 = (double)f2;
-            return v1 >= v2;
-        }
-        public static bool operator <=(Fraction f1, Fraction f2)
-        {
-            double v1 = (double)f1;
-            double v2 = (double)f2;
-            return v1 <= v2;
-        }
-
-        public static bool operator ==(Fraction f1, Fraction f2)
-        {
-            double v1 = (double)f1;
-            double v2 = (double)f2;
-            return v1 == v2;
-        }
-
-        public static bool operator !=(Fraction f1, Fraction f2)
-        {
-            double v1 = (double)f1;
-            double v2 = (double)f2;
-            return v1 != v2;
-        }
-
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var f = new Fraction(3, 5);
-
-            Fraction test = new Fraction(4, 10);
-            Fraction f3 = f - new Fraction(36,10);
-            Console.WriteLine(((Fraction) (-3.07)).Numerator);
 
         }
     }
+    
 }
